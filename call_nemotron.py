ad9c1bce97c80b9801ae5b5e69768c2672595199
kartik baderiya <baderiyakartik24@gmail.com>
2024-11-11 19:04:55 +0530

    torch issue

---
+++ call_nemotron.py
@@ -0,0 +1,22 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
+
+model_name = "nvidia/Llama-3.1-Nemotron-70B-Instruct"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto")
+
+def query_model(input_text):
+    inputs = tokenizer(input_text, return_tensors="pt")
+
+    with torch.no_grad():
+        outputs = model.generate(inputs['input_ids'], max_length=50, num_return_sequences=1)
+
+    response = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return response
+
+while True:
+    input_text = input("Enter your question (or 'exit' to stop): ")
+    if input_text.lower() == 'exit':
+        break
+    response = query_model(input_text)
+    print("Response:", response)
