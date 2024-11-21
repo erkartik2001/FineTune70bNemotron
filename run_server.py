832835db050cbce1040e1b772e42dcc4054c8a9f
kartik baderiya <baderiyakartik24@gmail.com>
2024-11-21 17:23:22 +0530

    added finetuining api

--- run_server.py
+++ run_server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from llm_model import LLMModel
 from flask_httpauth import HTTPBasicAuth
+import os
 
 app = Flask(__name__)
 auth = HTTPBasicAuth()
@@ -16,9 +17,9 @@ def get_pw(username):
     return None
 
 llm_model = LLMModel("nvidia/Llama-3.1-Nemotron-70B-Instruct-HF")
-llm_model.load_model()
+llm_model.load_model(model_path="./fine_tuned_model")
 
-@app.route('/api/call70b', methods=['POST'])
+@app.route('/api/call70b', methods=['POST','GET'])
 @auth.login_required
 def predict():
     data = request.json
@@ -27,6 +28,36 @@ def predict():
     response_text = llm_model.query_model(prompt)
     
     return jsonify({"response": response_text})
+
+
+@app.route('/api/finetune70b',methods=["POST","GET"])
+@auth.login_required
+def finetune70b():
+    if 'file' not in request.files:
+        return jsonify({"error":"No file part in request"}), 400
+    
+    file = request.files["file"]
+
+    if file.filename == "":
+        return jsonify({"error":"No file selected"}), 400
+    
+    if not file.filename.endswith(".txt"):
+        return jsonify({"error":"Only .txt files are allowed"}), 400
+    
+    file_path = os.path.join("temp",file.filename)
+    os.makedirs("temp",exist_ok=True)
+    file.save(file_path)
+
+    try:
+        llm_model.fine_tune(train_file=file_path)
+        return jsonify({"message":"Training completed"}), 200
+    
+    except Exception as e:
+        return jsonify({"error":str(e)}), 500
+    
+    finally:
+        os.remove(file_path)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
